a16edae0e587012c317116e8b2b0006f0f636223
manojjoinme5 <63599747+manojjoinme5@users.noreply.github.com>
2020-04-14 11:45:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 </h1>
 </body>
 
-<!---
+
 <body bgcolor=green text=red>
 <h1> 
 <marquee direction="right" behavior="alternate" scrollamount="20">Green Deployment </marquee>
 </h1>
 </body>
---->
+
 
 </html>
